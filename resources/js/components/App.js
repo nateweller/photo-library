54de8f6d49211851fbd92dcbde05496e1d3ec061
Nate Weller <hello@nateweller.com>
2020-02-05 19:15:18 -0500

    add react-router-dom and init routing structure

--- resources/js/components/App.js
+++ resources/js/components/App.js
@@ -1,14 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+import Home from '../pages/Home';
 
 export default class App extends React.Component {
 	render () {
 		return (
-			<div>Hello, World!</div>
+			<>
+				<Switch>
+					<Route path="/" component={ Home } />
+				</Switch>
+			</>
 		);
 	};
 }
 
 if (document.getElementById('app')) {
-    ReactDOM.render(<App />, document.getElementById('app'));
+    ReactDOM.render(
+		<BrowserRouter>
+			<App />
+		</BrowserRouter>, 
+		document.getElementById('app')
+	);
 }
