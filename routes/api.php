f877d640d3a12750095ebe1984e65e51eb7fc199
Nate Weller <hello@nateweller.com>
2020-02-25 17:18:13 -0700

    scaffold API routes

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,48 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::post('/auth', function (Request $request) {
+    // to do: validate sign in and return api token
+    return ['foo' => 'bar'];
+});
+
+Route::get('/photos', function (Request $request) {
+    // to do: single photo result via ID
+    // to do: array of photos result via search params:
+        // keyword search
+        // collection filter
+        // date filters
+        // photographer search
+        // status filter
+        // size filters
+        // model search
+    return ['foo' => 'bar'];
+});
+
+Route::middleware('auth:api')->post('/photos', function (Request $request) {
+    // to do: photo update via ID(s)
+    // to do: photo insert
+    return ['foo' => 'bar'];
+});
+
+Router::middleware('auth:api')->get('/settings', function (Request $request) {
+    // to do: get setting value by key
+    return ['foo' => 'bar'];
+});
+
+Router::middleware('auth:api')->post('/settings', function (Request $request) {
+    // to do: update setting value by key
+    return ['foo' => 'bar'];
+});
+
+Router::middleware('auth:api')->get('/users', function (Request $request) {
+    // to do: single user via ID
+    // to do: list of users
+    return ['foo' => 'bar'];
+});
+
+Router::middleware('auth:api')->post('/users', function (Request $request) {
+    // to do: update user via ID
+    // to do: insert new user
+    return ['foo' => 'bar'];
 });
